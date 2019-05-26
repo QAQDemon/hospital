fba0aef3a2084978b9ee8c2afe68cd703d72721e
QAQDemon <39302989+QAQDemon@users.noreply.github.com>
2019-05-26 20:34:43 +0800

    Revert "Initial commit"
    This reverts commit 8ce2bf3ae819bee007aaad9a786c90d43b418b4e.

--- src/main/java/edu/neu/medical/hospital/service/impl/RegistrationInfoServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package edu.neu.medical.hospital.service.impl;
-
-import edu.neu.medical.hospital.dao.RegistrationInfoMapper;
-import edu.neu.medical.hospital.service.RegistrationInfoService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class RegistrationInfoServiceImpl implements RegistrationInfoService {
-}
