8ce2bf3ae819bee007aaad9a786c90d43b418b4e
QAQDemon <39302989+QAQDemon@users.noreply.github.com>
2019-05-26 20:25:01 +0800

    Initial commit

---
+++ src/main/java/edu/neu/medical/hospital/service/RegistrationInfoService.java
@@ -0,0 +1,4 @@
+package edu.neu.medical.hospital.service;
+
+public interface RegistrationInfoService {
+}
---
+++ src/main/java/edu/neu/medical/hospital/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,8 @@
+package edu.neu.medical.hospital.service.impl;
+
+import edu.neu.medical.hospital.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+}
